2755bf6bb5faacf0c47de17b47fbec7c9557a407
Andrew Garcia <garcia.gtr@gmail.com>
2025-11-10 22:46:31 -0500

    this expands the fur status command ; extends to git status if .git version controlled

---
+++ src/utils/git.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+/// Finds the nearest .git directory (Git's real behavior)
+pub fn find_git_root() -> Option<PathBuf> {
+    let mut dir = std::env::current_dir().ok()?;
+
+    loop {
+        if dir.join(".git").is_dir() {
+            return Some(dir);
+        }
+
+        if !dir.pop() {
+            break;
+        }
+    }
+
+    None
+}
+
